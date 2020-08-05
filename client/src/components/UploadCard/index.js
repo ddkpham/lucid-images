1f498c1b30103c781d3f400761326d3139dcc36c
David Pham <davidpham@Davids-MacBook-Pro.local>
2020-08-04 18:35:57 -0700

    added flask route

--- client/src/components/UploadCard/index.js
+++ client/src/components/UploadCard/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
@@ -7,7 +7,6 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-
 import "./index.scss";
 
 const useStyles = makeStyles({
@@ -17,7 +16,22 @@ const useStyles = makeStyles({
 });
 
 export default function ImgMediaCard() {
+  const [file, setFile] = useState("");
+  const [currImage, setCurrImage] = useState("");
   const classes = useStyles();
+
+  const fileSelectedHandler = (event) => {
+    console.log("ImgMediaCard -> event", event.target.files[0]);
+    const file = event.target.files[0];
+    setFile(file.name);
+  };
+
+  const uploadImage = () => {
+    const basePath = "/home/vagrant/project/images";
+    const fullPathToImage = `${basePath}/${file}`;
+    setCurrImage(fullPathToImage);
+    console.log("uploadImage -> fullPathToImage", fullPathToImage);
+  };
 
   return (
     <Card className={classes.root}>
@@ -26,17 +40,19 @@ export default function ImgMediaCard() {
           component="img"
           alt="Contemplative Reptile"
           height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
+          image={currImage}
           title="Contemplative Reptile"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
-            Choose a photo that needs contrast enhancement
+            Please select files in the images folder of the project root
+            directory.
           </Typography>
+          <input type="file" onChange={fileSelectedHandler} />
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary">
+        <Button size="small" color="primary" onClick={uploadImage}>
           Upload
         </Button>
         <Button size="small" color="primary">
